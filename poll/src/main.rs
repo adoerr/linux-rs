4063b7bd8ef90d432680ba1a6d339872d79a35d0
adoerr <0xad@gmx.net>
2024-03-12 06:18:08 +0100

    send delay request
    Signed-off-by: adoerr <0xad@gmx.net>

--- poll/src/main.rs
+++ poll/src/main.rs
@@ -1,11 +1,34 @@
 #![allow(dead_code)]
 
-use std::io::Result;
+use std::{
+    io::{Result, Write},
+    net::TcpStream,
+};
 
 mod poll;
 mod poll_sys;
 
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Request {
+    delay_ms: u64,
+    message: String,
+}
+
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("127.0.0.1:4242")?;
+
+    let request = Request {
+        delay_ms: 100,
+        message: "Hello, world!".to_string(),
+    };
+
+    send(&request, &mut stream)?;
+
+    Ok(())
+}
+
+fn send(request: &Request, stream: &mut TcpStream) -> Result<()> {
+    let request = serde_json::to_string(request)?;
+    stream.write_all(request.as_bytes())?;
     Ok(())
 }
