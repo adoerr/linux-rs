994db42715bb68ad498b775291f4521c2827b582
adoerr <0xad@gmx.net>
2024-03-12 06:52:59 +0100

    get rid of the web stuff
    Signed-off-by: adoerr <0xad@gmx.net>

--- tools/delay/src/main.rs
+++ tools/delay/src/main.rs
@@ -1,28 +1,25 @@
 use std::{
-    sync::atomic::{AtomicUsize, Ordering},
-    time::Duration,
+    io::{Read, Result},
+    net::{TcpListener, TcpStream},
 };
 
-use actix_web::{get, rt::time::sleep, web, App, HttpServer, Responder};
-
-static COUNTER: AtomicUsize = AtomicUsize::new(1);
-
-#[get("/{delay}/{message}")]
-async fn delay(path: web::Path<(u64, String)>) -> impl Responder {
-    let (delay_ms, message) = path.into_inner();
-    let count = COUNTER.fetch_add(1, Ordering::SeqCst);
-
-    println!("#{count} - {delay_ms}ms: {message}");
-
-    sleep(Duration::from_millis(delay_ms)).await;
-
-    message
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Request {
+    delay_ms: u64,
+    message: String,
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(delay))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4242")?;
+    let (mut stream, _) = listener.accept()?;
+    let request = read(&mut stream)?;
+    println!("{:?}", request);
+    Ok(())
+}
+
+fn read(stream: &mut TcpStream) -> Result<Request> {
+    let mut buf = [0; 1024];
+    let n = stream.read(&mut buf)?;
+    let request = serde_json::from_slice(&buf[..n])?;
+    Ok(request)
 }
