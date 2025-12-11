f762e7372cc0fc53bca01cd83c0bc0d117b8511d
adoerr <0xad@gmx.net>
2025-12-11 18:00:52 +0100

    list wifi interfaces
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ wifi/examples/list_interfaces.rs
@@ -0,0 +1,18 @@
+use netlink_wi::NlSocket;
+
+fn main() {
+    env_logger::try_init().unwrap();
+
+    let socket = NlSocket::connect().unwrap();
+    let interfaces = socket.list_interfaces().unwrap();
+
+    for i in interfaces {
+        if !i.name.is_empty() {
+            println!(
+                "Interface: {}, SSID: {}",
+                i.name,
+                i.ssid.unwrap_or_else(|| "n/a".to_string())
+            );
+        }
+    }
+}
