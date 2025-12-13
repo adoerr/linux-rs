1bb3b886151e36b8d2d86371b7dce190d48bd18c
adoerr <0xad@gmx.net>
2025-12-14 00:37:49 +0100

    refactor: interface listing
    Signed-off-by: adoerr <0xad@gmx.net>

--- wifi/examples/list_interfaces.rs
+++ wifi/examples/list_interfaces.rs
@@ -7,21 +7,22 @@ fn main() {
     let interfaces = socket.list_interfaces().unwrap();
 
     for i in interfaces {
-        if !i.name.is_empty() {
-            println!(
-                "Interface: {}, SSID: {}",
-                i.name,
-                i.ssid.unwrap_or_else(|| "n/a".to_string())
-            );
+        if i.name.is_empty() {
+            continue;
+        }
 
-            let stations = socket.list_stations(i.interface_index).unwrap();
-            for s in stations {
-                println!(
-                    "    Station: {}, Signal: {} dBm",
-                    s.mac,
-                    s.signal.unwrap_or(0)
-                );
-            }
+        println!(
+            "Interface: {}, SSID: {}",
+            i.name,
+            i.ssid.unwrap_or_else(|| "n/a".to_string())
+        );
+
+        for s in socket.list_stations(i.interface_index).unwrap() {
+            println!(
+                "    Station: {}, Signal: {} dBm",
+                s.mac,
+                s.signal.unwrap_or(0)
+            );
         }
     }
 }
