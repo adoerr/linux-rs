7663d3d1030a8f4c8b77621e908de36c8cddb05d
adoerr <0xad@gmx.net>
2026-01-24 11:06:56 +0100

    Start crate `agent`
    Signed-off-by: adoerr <0xad@gmx.net>

--- agent/src/lib.rs
+++ agent/src/lib.rs
@@ -1,3 +1,8 @@
+#![allow(unused)]
+
+mod error;
+pub use error::{Error, Result};
+
 /// System call wrapper.
 ///
 /// Wrapper around `poll_sys` calls that checks `errno` on failure.
@@ -5,7 +10,7 @@
 macro_rules! syscall {
     ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
         #[allow(clippy::macro_metavars_in_unsafe)]
-        let res = unsafe { poll_sys::$fn($($arg, )*) };
+        let res = unsafe { libc::$fn($($arg, )*) };
 
         if res == -1 {
             Err(std::io::Error::last_os_error())
