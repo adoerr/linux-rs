edf137799c7655351989d744299543f6c3ed4b81
adoerr <0xad@gmx.net>
2026-03-16 16:01:22 +0100

    Connect and get registry
    Signed-off-by: adoerr <0xad@gmx.net>

--- wayland/src/error.rs
+++ wayland/src/error.rs
@@ -1,7 +1,17 @@
+use wayland_client::{ConnectError, DispatchError};
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    /// Wayland connection error
+    #[error("Wayland connection error {0}")]
+    Connect(#[from] ConnectError),
+
+    /// Wayland dispatching error
+    #[error("Wayland dispatching error {0}")]
+    Dispatch(#[from] DispatchError),
+
     /// Other error
     #[error(transparent)]
     Other(#[from] anyhow::Error),
--- wayland/src/main.rs
+++ wayland/src/main.rs
@@ -1,11 +1,61 @@
+#![allow(dead_code, unused_imports, unused_variables)]
+
 mod error;
 
 use error::Result;
+use wayland_client::{
+    Connection, Dispatch, EventQueue, QueueHandle,
+    protocol::{
+        wl_buffer, wl_registry, wl_registry::WlRegistry, wl_surface, wl_surface::WlSurface,
+    },
+};
+use wayland_protocols::xdg::shell::client::{xdg_surface, xdg_toplevel, xdg_wm_base};
+
+struct State {
+    running: bool,
+    surface: Option<wl_surface::WlSurface>,
+    buffer: Option<wl_buffer::WlBuffer>,
+    wm_base: Option<xdg_wm_base::XdgWmBase>,
+    xdg_surface: Option<(xdg_surface::XdgSurface, xdg_toplevel::XdgToplevel)>,
+    configured: bool,
+}
 
 fn main() -> Result<()> {
     env_logger::init();
 
-    log::info!("Hello world!");
+    let conn = Connection::connect_to_env()?;
+    let mut event_queue: EventQueue<State> = conn.new_event_queue();
+    let queue_handle = event_queue.handle();
+    let display = conn.display();
+    display.get_registry(&queue_handle, ());
+
+    let mut state = State {
+        running: true,
+        surface: None,
+        buffer: None,
+        wm_base: None,
+        xdg_surface: None,
+        configured: false,
+    };
+
+    println!("Starting example window app, press <ESC> to quit.");
+
+    while state.running {
+        event_queue.blocking_dispatch(&mut state)?;
+    }
 
     Ok(())
+}
+
+impl Dispatch<wl_registry::WlRegistry, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        log::debug!("<- {:?}", event);
+    }
 }
